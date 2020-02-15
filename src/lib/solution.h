148d49de415d74846b1a76bef40a993c82037fc1
Sam Bruner <sambruner1@gmail.com>
2020-02-14 17:29:47 -0800

    Initial Commit

--- src/lib/solution.h
+++ src/lib/solution.h
@@ -6,19 +6,13 @@
 #include <vector>
 
 // Person class is defined for demonstration
-struct Person {
-  std::string first_name;
-  std::string last_name;
-  int age;
-};
+
 
 class Solution {
 public:
-  std::string PrintHelloWorld();
-  Person *ReturnInvalidPointer();
-  int fib(int N);
+
 private:
-  std::map<int, int> _m;
+
 };
 
 #endif
